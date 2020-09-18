238b3b4fce5dc50aac06aac099fc23574a2c37f3
jamessun-github <68424789+jamessun-github@users.noreply.github.com>
2020-09-18 05:37:32 -0500

    Update PostSoapMessageOBTest.java
    Update

--- src/test/java/com/jbase/CNMB/TITSOAP/PostSoapMessageOBTest.java
+++ src/test/java/com/jbase/CNMB/TITSOAP/PostSoapMessageOBTest.java
@@ -15,12 +15,12 @@ public class PostSoapMessageOBTest {
 
         
         //String sUrl = "http://localhost:8090/CHBTWS/services";
-        String sUrl = "http://10.252.241.196:7800/esbws?wsdl";
+        String sUrl = "http://127.0.0.1:7800/esbws?wsdl";
         String sContentType = "text/xml; charset=utf-8";
         String timeoutStr = "60";
         String sRetry = "3";
-        String soapRequestData = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?> <soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:chb=\"http://temenos.com/CHBTWS\">   <soapenv:Header/>   <soapenv:Body>      <chb:callOfs>           <OfsRequest>ENQUIRY.SELECT,,ITFIB.01//HK0010256,ENQ.ITF.LOAN.BAL,ACCOUNT:EQ=256500001201</OfsRequest>      </chb:callOfs>   </soapenv:Body></soapenv:Envelope>";
-        //String soapRequestData = "  <soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:chb=\"http://temenos.com/CHBTWS\">   <soapenv:Header/>   <soapenv:Body>      <chb:callOfs>           <OfsRequest>ENQUIRY.SELECT,,ITFIB.01//HK0010256,ENQ.ITF.LOAN.BAL,ACCOUNT:EQ=256500001201</OfsRequest>      </chb:callOfs>   </soapenv:Body></soapenv:Envelope>";
+        String soapRequestData = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?> <soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:chb=\"http://test.com/CHBTWS\">   <soapenv:Header/>   <soapenv:Body>      <chb:callOfs>           <OfsRequest>ENQUIRY.SELECT,,ITFIB.01//HK0010256,ENQ.ITF.LOAN.BAL,ACCOUNT:EQ=256500001201</OfsRequest>      </chb:callOfs>   </soapenv:Body></soapenv:Envelope>";
+        //String soapRequestData = "  <soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:chb=\"http://test.com/CHBTWS\">   <soapenv:Header/>   <soapenv:Body>      <chb:callOfs>           <OfsRequest>ENQUIRY.SELECT,,ITFIB.01//HK0010256,ENQ.ITF.LOAN.BAL,ACCOUNT:EQ=256500001201</OfsRequest>      </chb:callOfs>   </soapenv:Body></soapenv:Envelope>";
 
         String strDelimit = "TITSOAPSPLIT";
         String dynArray =  strDelimit + strDelimit + strDelimit + timeoutStr + strDelimit + sRetry + strDelimit + strDelimit+strDelimit+strDelimit+ sContentType+ strDelimit+ sUrl + strDelimit+ soapRequestData;
